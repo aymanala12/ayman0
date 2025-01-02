82c5747f09c3254ec476dfd09442570828189de0
aymanala12 <aymanala0002011@gmail.com>
2025-01-02 19:29:06 +0200

    aaa

---
+++ ayman.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+   
+int x=1;
+for(x;x<11;x++)
+{
+  if (x%2==0){
+  printf("%d",x);
+  printf("   even\n");
+}else {
+  printf("%d",x);
+  printf("   odd\n");
+}
+}
+ return (0);
+}

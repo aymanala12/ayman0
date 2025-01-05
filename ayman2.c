978d4c8ce10989ea3c82096db102a77d911bea2e
aymanala12 <aymanala0002011@gmail.com>
2025-01-05 21:43:05 +0200

    ayman2.c

---
+++ ayman2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main () {
+int x = 1 ;
+int y = 1 ;
+int z = 1 ;
+int a = 1 ;
+
+ for (a;a<11;a++) {
+    printf ("#");
+    }
+    printf ("\n");
+
+for(x;x<4;x++) {
+
+    for(y;y<11;y++){
+ switch (z){
+    case 1 :
+    printf ("#"); z++ ;
+     break ; 
+     case 10 :
+     printf ("#");  z++ ;
+     break ; 
+     default :
+     printf ("*");  z++ ;
+
+} } y = 1;  z =1 ;
+printf ("\n");
+   
+}
+ for(a;a<22;a++) {
+    printf("#") ;}
+    printf("\n") ;
+
+
+ return(0);
+}

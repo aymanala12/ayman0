4fc3fc930225f0b055248f1c0b987d704f7b6af7
aymanala12 <aymanala0002011@gmail.com>
2025-01-13 01:53:12 +0200

    Add files via upload

---
+++ ayman4.c
@@ -0,0 +1,11 @@
+    #include <stdio.h>
+    int main () {
+
+
+    int i;
+    int x;
+    printf (" please: just writte the two numbers that need to be subtracted or added to each other's \n");
+    scanf("%d",&i);
+    scanf("%d",&x);
+    printf("%d",x+i);
+        return(0);}
